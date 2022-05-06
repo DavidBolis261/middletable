493ae332a6ebd4a75ee3b2f03b18c5db2e275943
Ronan Konstantin <ronankonstantin@gmail.com>
2022-05-06 20:10:20 +1000

    Fixed player movement issues, added reset functionality

--- Assets/Scripts/StatusManager.cs
+++ Assets/Scripts/StatusManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
 
 public class StatusManager : MonoBehaviour
 {
@@ -10,20 +11,36 @@ public class StatusManager : MonoBehaviour
 
     private int movesDone = 0;
 
+    private List<Transform> players = new List<Transform>();
+
     // Start is called before the first frame update
     void Start()
     {
         instance = this;
+        foreach(GameObject p in GameObject.FindGameObjectsWithTag("Player"))
+        {
+            players.Add(p.transform);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.R)) Reset();
     }
 
     public void LevelCompleted()
     {
         SceneManager.LoadScene("Main Menu");
+    }
+
+    public void Reset()
+    {
+        foreach(Transform p in players)
+        {
+            if(DOTween.IsTweening(p.transform))
+                return;
+        }
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
